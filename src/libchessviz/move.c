0899b04555433b49a9d4fbe6b20023fb917c5f79
chaz3R <pr0ger@internet.ru>
2022-04-29 10:00:30 +0700

    Added correct first move check

--- src/libchessviz/move.c
+++ src/libchessviz/move.c
@@ -8,17 +8,17 @@ void move(char board[board_size][board_size], int x1, int y1, int x2, int y2)
     board[y1][x1] = ' ';
 }
 
-void move_pawn(char board[board_size][board_size], int x1, int y1, int x2, int y2, int P_cnt_moves, int p_cnt_moves)
+void move_pawn(char board[board_size][board_size], int x1, int y1, int x2, int y2)
 {
     if (board[y2][x2] == ' ' && (x1==x2))
     {
         if(board[y1][x1] == 'P' && (y1>y2))
         {
-            if (P_cnt_moves == 0 && (abs(y1-y2) == 2 || abs(y1-y2) == 1))
+            if (y1 == 6 && (abs(y1-y2) == 2 || abs(y1-y2) == 1))
             {
                 move(board, x1, y1, x2, y2);
             }
-            else if (P_cnt_moves > 0 && abs(y1-y2) == 1)
+            else if (abs(y1-y2) == 1)
             {
                 move(board, x1, y1, x2, y2);
             }
@@ -29,11 +29,11 @@ void move_pawn(char board[board_size][board_size], int x1, int y1, int x2, int y
         }
         else if(board[y1][x1] == 'p' && (y2>y1))
         {
-            if (p_cnt_moves == 0 && (abs(y1-y2) == 2 || abs(y1-y2) == 1))
+            if (y1 == 1 && (abs(y1-y2) == 2 || abs(y1-y2) == 1))
             {
                 move(board, x1, y1, x2, y2);
             }
-            else if (p_cnt_moves > 0 && abs(y1-y2) == 1)
+            else if (abs(y1-y2) == 1)
             {
                 move(board, x1, y1, x2, y2);
             }
--- src/libchessviz/move.h
+++ src/libchessviz/move.h
@@ -3,4 +3,4 @@
 
 void move(char board[board_size][board_size], int x1, int y1, int x2, int y2);
 
-void move_pawn(char board[board_size][board_size], int x1, int y1, int x2, int y2, int P_cnt_moves, int p_cnt_moves);
+void move_pawn(char board[board_size][board_size], int x1, int y1, int x2, int y2);

53aa33731ce46914419351a8f35dc227e354acf7
Egor Urlov <pr0ger@internet.ru>
2022-05-20 14:55:46 +0700

    fix codestyle

--- test/board_test.c
+++ test/board_test.c
@@ -5,14 +5,14 @@
 #define n 5
 
 char board[board_size][board_size]
-            = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-               {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-               {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-               {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+        = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+           {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+           {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+           {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
 int x1, y11, x2, y2;
 
@@ -26,17 +26,16 @@ CTEST(ctest, write1)
     input[2] = '-';
     input[3] = 'e';
     input[4] = '4';
-    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u'
-            && input[3] != 'i' && input[4] != 't') {
-            x1 = rightX(input[0]);
-            y11 = rightY(input[1]);
+    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u' && input[3] != 'i'
+        && input[4] != 't') {
+        x1 = rightX(input[0]);
+        y11 = rightY(input[1]);
 
-            x2 = rightX(input[3]);
-            y2 = rightY(input[4]);
+        x2 = rightX(input[3]);
+        y2 = rightY(input[4]);
 
-            flag = movecheck(board, x1, y11, x2, y2);
-    }
-    else{
+        flag = movecheck(board, x1, y11, x2, y2);
+    } else {
         flag = 1;
     }
 
@@ -53,17 +52,16 @@ CTEST(ctest, write2)
     input[2] = '-';
     input[3] = 'e';
     input[4] = '5';
-    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u'
-            && input[3] != 'i' && input[4] != 't') {
-            x1 = rightX(input[0]);
-            y11 = rightY(input[1]);
+    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u' && input[3] != 'i'
+        && input[4] != 't') {
+        x1 = rightX(input[0]);
+        y11 = rightY(input[1]);
 
-            x2 = rightX(input[3]);
-            y2 = rightY(input[4]);
+        x2 = rightX(input[3]);
+        y2 = rightY(input[4]);
 
-            flag = movecheck(board, x1, y11, x2, y2);
-    }
-    else{
+        flag = movecheck(board, x1, y11, x2, y2);
+    } else {
         flag = 1;
     }
 
@@ -80,17 +78,16 @@ CTEST(ctest, write3)
     input[2] = '-';
     input[3] = 'd';
     input[4] = '3';
-    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u'
-            && input[3] != 'i' && input[4] != 't') {
-            x1 = rightX(input[0]);
-            y11 = rightY(input[1]);
+    if (input[0] != '#' && input[1] != 'q' && input[2] != 'u' && input[3] != 'i'
+        && input[4] != 't') {
+        x1 = rightX(input[0]);
+        y11 = rightY(input[1]);
 
-            x2 = rightX(input[3]);
-            y2 = rightY(input[4]);
+        x2 = rightX(input[3]);
+        y2 = rightY(input[4]);
 
-            flag = movecheck(board, x1, y11, x2, y2);
-    }
-    else{
+        flag = movecheck(board, x1, y11, x2, y2);
+    } else {
         flag = 1;
     }
 
